848f6f70bd8e8b21db08708803fecb149d34239f
Sinclebear <sanghyukj@gmail.com>
2022-01-25 22:42:28 +0900

    ec2 데이터베이스 테스트

--- schemas/index.js
+++ schemas/index.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const connect = () => {
     // ignoreUndefined: .. undefined로 넘어오는 값은무시 하도록 설정
+    // local test의 경우
     // mongoose.connect("mongodb://localhost:27017/spa_database", { ignoreUndefined: true }).catch((err) => {
-        mongoose.connect("mongodb://localhost:27017/blog_database", { ignoreUndefined: true }).catch((err) => {
+    // EC2 ubuntu 의 경우
+        mongoose.connect("mongodb://test:test@localhost:27017/blog_database", { ignoreUndefined: true }).catch((err) => {
         console.error(err)
     });
 };
