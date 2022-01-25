152ab6e3b90e568190a524537d45432681e6c06f
Sinclebear <sanghyukj@gmail.com>
2022-01-26 01:03:16 +0900

    db ...

--- schemas/index.js
+++ schemas/index.js
@@ -5,10 +5,7 @@ const connect = () => {
     // local test의 경우
     // mongoose.connect("mongodb://localhost:27017/blog_database", { ignoreUndefined: true }).catch((err) => {
     // EC2 ubuntu 의 경우
-        mongoose.connect("mongodb://localhost:27017/blog_database", {
-            "auth": {"authSource": "admin"},
-            "user": "test",
-            "pass": "test"}, 
+        mongoose.connect("mongodb://test:test@localhost:27017/blog_database?authSource=admin&w=1", {
             { ignoreUndefined: true }).catch((err) => {
         console.error(err)
     });
