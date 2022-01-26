0a70040fb08da3fac86d1a9993420110bb49e989
Sinclebear <sanghyukj@gmail.com>
2022-01-27 01:12:48 +0900

    mongoose-sequence 추가하여 articleId를 자동으로 증가하도록함

--- schemas/blog.js
+++ schemas/blog.js
@@ -1,12 +1,15 @@
+const autoIdSetter = require('./auto-id-setter');
 const mongoose = require("mongoose");
+
+
 
 const blogSchema = mongoose.Schema(
     {
-        articleId: {
-            type: Number,
-            required: true,
-            unique: true,
-        },
+        // articleId: {
+        //     type: Number,
+        //     required: true,
+        //     unique: true,
+        // },
         title: {
             type:String,
             required: true,
@@ -30,5 +33,6 @@ const blogSchema = mongoose.Schema(
     },
     { timestamps: true }
 );
-
+// POST 요청이 들어올 시 articles 콜렉션에 articleId 가 자동 증가한 값을 추가한다 
+autoIdSetter(blogSchema, mongoose, 'article', 'articleId'); 
 module.exports = mongoose.model("Articles", blogSchema);
