b7952a207fdd3bc8d2b8fd2cb27c9e0f4a80dcda
Sinclebear <sanghyukj@gmail.com>
2022-01-27 01:57:49 +0900

    EC2 ubuntu 데이터베이스 연결 대상으로 변경

--- schemas/index.js
+++ schemas/index.js
@@ -4,12 +4,12 @@ const connect = () => {
     // ignoreUndefined: .. undefined로 넘어오는 값은무시 하도록 설정
 
     // local test의 경우
-     mongoose.connect("mongodb://localhost:27017/blog_database", { ignoreUndefined: true }).catch((err) => {
+    // mongoose.connect("mongodb://localhost:27017/blog_database", { ignoreUndefined: true }).catch((err) => {
          
     // EC2 ubuntu 의 경우
-        // mongoose.connect("mongodb://test:test@localhost:27017/blog_database?authSource=admin&w=1",
-        //     { ignoreUndefined: true }).catch((err) => {
-        // console.error(err)
+        mongoose.connect("mongodb://test:test@localhost:27017/blog_database?authSource=admin&w=1",
+            { ignoreUndefined: true }).catch((err) => {
+        console.error(err)
     });
 };
 
